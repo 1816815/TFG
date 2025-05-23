2eae777fbe5ceab10e24007b5ff5952ee072abff
1816815 <166591362+1816815@users.noreply.github.com>
2025-05-23 12:22:39 +0200

    Update settings.py

--- Back/TFG/mi_tfg/settings.py
+++ Back/TFG/mi_tfg/settings.py
@@ -54,6 +54,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://91.99.94.146:6002".
+    "http://cuestamarket.duckdns.org:6002/"
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
