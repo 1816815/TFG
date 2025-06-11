6a254d3b1bcaed85a7db1385664367e171e524f6
1816815 <1816815@alu.murciaeduca.es>
2025-06-11 17:53:31 +0200

    Ajustes de despliegue

--- Back/TFG/mi_tfg/settings_production.py
+++ Back/TFG/mi_tfg/settings_production.py
@@ -1,13 +1,15 @@
-from decouple import config, Csv
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+import os 
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cuestamarket.duckdns.org', 'api.cuestamarket.duckdns.org']
 DEBUG = False
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('MYSQL_DATABASE'),
-        'USER': config('MYSQL_USER'),
-        'PASSWORD': config('MYSQL_PASSWORD'),
-        'HOST': config('MYSQL_HOST'),
-        'PORT': config('MYSQL_PORT')
+        'NAME': os.getenv('MARIADB_DATABASE'),
+        'USER': os.getenv('MARIADB_USER'),
+        'PASSWORD': os.getenv('MARIADB_PASSWORD'),
+        'HOST': os.getenv('MARIADB_HOST'),
+        'PORT': os.getenv('MARIADB_PORT')
     }
 }
